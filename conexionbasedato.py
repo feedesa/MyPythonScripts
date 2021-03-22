6003b7c048754a19fdf7968898f77f565345e3ff
feedesa <federesali@hotmail.com>
2021-03-22 00:05:23 -0300

    bbdd

--- conexionbasedato.py
+++ conexionbasedato.py
@@ -1,26 +1,30 @@
 import pymysql
 
-class Socios:
-    def abrir():
-
-
-        bbdd= pymysql.connect( host= "localhost", user= "root", passwd="", db= "ejemplo1")ef
+class Socios():
+    def abrir(self):
+        bbdd= pymysql.connect( host= "localhost", user= "root", passwd="", db= "ejemplo1")
+        return bbdd
 
     def alta(self,datos):
-        cone=self.abrir()
-        cursor=cone.cursor()
-        sql = "INSERT INTO SOCIOS ( id, nombre, SEXO)\
-            values( '(0)','(1)','(2)')".format(self.entrynumerodesocio.get(),self.entrynombre.get(),"H" )
-
+        '''
+        datos[0]: id
+        datos[1]: nombre
+        '''
+        bbdd=self.abrir()
+        cursor=bbdd.cursor()
+        sql = "INSERT INTO SOCIOS ( id, nombre)\
+            values( '({})','{}')".format(datos[0],datos[1] )
+        print (sql)
+        #sql="insert into articulos(descripcion, precio) values (%s,%s)"
         try:
             cursor.execute(sql)
             bbdd.commit()
-            messagebox.showinfo(message = "registro exitoso", title = "Aviso")
+            #messagebox.showinfo(message = "registro exitoso", title = "Aviso")
         except:
             bbdd.rollback()
-            messagebox.showinfo(message= "No registrado", title = "Aviso"
+            #messagebox.showinfo(message= "No registrado", title = "Aviso" )
 
-            bbdd.close()
+        bbdd.close()
 
 
 
